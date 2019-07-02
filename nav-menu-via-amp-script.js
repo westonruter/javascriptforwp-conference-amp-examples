2749a9aa9c6034746bba6ac34c8b3e24c267db63
Weston Ruter <westonruter@google.com>
2019-07-02 12:13:11 -0700

    Update button text to reflect button state

--- nav-menu-via-amp-script.js
+++ nav-menu-via-amp-script.js
@@ -1,10 +1,13 @@
-const toggleNavMenu = document.getElementById( 'toggleNavMenu' );
+const button = document.getElementById( 'toggleNavMenu' );
 const navMenu = document.getElementById( 'navMenu' );
 
-toggleNavMenu.addEventListener( 'click', () => {
+button.addEventListener( 'click', () => {
     // TODO: Replace with navMenu.hidden once closed: https://github.com/ampproject/amphtml/issues/23156
     const hidden = ! navMenu.classList.contains( 'hidden' );
     navMenu.classList.toggle( 'hidden', hidden );
 
-    toggleNavMenu.setAttribute( 'aria-expanded', hidden ? 'false' : 'true' );
+    button.setAttribute( 'aria-expanded', hidden ? 'false' : 'true' );
+
+    // TODO: Use dataset once it is supported.
+    button.textContent = hidden ? button.getAttribute( 'data-open-text' ) : button.getAttribute( 'data-close-text' );
 } );
