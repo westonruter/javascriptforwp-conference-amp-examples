242dcb1e2850e17b663a386f4e3a8b8adecc6bba
Weston Ruter <westonruter@google.com>
2019-07-02 11:52:05 -0700

    Work around bug with hidden prop

--- nav-menu-via-amp-script.js
+++ nav-menu-via-amp-script.js
@@ -2,5 +2,9 @@ const toggleNavMenu = document.getElementById( 'toggleNavMenu' );
 const navMenu = document.getElementById( 'navMenu' );
 
 toggleNavMenu.addEventListener( 'click', () => {
-    navMenu.hidden = ! navMenu.hidden;
+    // TODO: Replace with navMenu.hidden once closed: https://github.com/ampproject/amphtml/issues/23156
+    const hidden = ! navMenu.classList.contains( 'hidden' );
+    navMenu.classList.toggle( 'hidden', hidden );
+
+    toggleNavMenu.setAttribute( 'aria-expanded', hidden ? 'false' : 'true' );
 } );
